18dfd907775b1dacaeaf2a0ca613f32484990ed1
vishal <vishal_s_yadav@yahoo.com>
2013-10-01 18:21:35 -0700

    Finished make_char_buffer function. Possibly. Maybe. Hopefully.

--- read-command.c
+++ read-command.c
@@ -22,9 +22,15 @@ char* make_char_buffer (int (*get_next_byte) (void *), void *get_next_byte_argum
   char* char_buffer = (char *) checked_malloc(buffer_size);   //initialize buffer, with size of 1024
   char curr_byte;
 
+  size_t file_index = 0;
   while ((curr_byte = get_next_byte(get_next_byte_argument)) != EOF) {
-    
+    char_buffer[file_index] = curr_byte;
+    file_index++;
+    if(file_index == buffer_size) {       //buffer has been filled, need to make it grow
+      char_buffer = (char *) checked_realloc(char_buffer, &buffer_size);
+    }
   }
+  return char_buffer;
 }
 
 command_stream_t
